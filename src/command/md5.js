f791e17c4b1ffb29629b30d9b49c7662376735b4
zp <zp@zhoup.top>
2026-01-13 15:15:50 +0800

    新增 md5 命令

--- src/command/md5.js
+++ src/command/md5.js
@@ -1,13 +1,24 @@
 const crypto = require('crypto');
 const { question } = require("../utils");
 
-async function main(options) {
-    let input = (await question('请输入要计算 MD5 的内容: ')).trim();
-    if (!input) return;
+async function main(options = {}) {
+    let input
+    if (options.content) {
+        input = options.content;
+    } else {
+        input = (await question('请输入内容: ')).trim();
+    }
 
     const iteration = Math.max(parseInt(options.iteration), 1);
     const length = Math.min(parseInt(options.length), 32);
-    const base64 = options.base64;
+    const isBase64 = options.base64;
+
+    // 输入内容验证
+    if (!input) return;
+    if (isNaN(iteration) || isNaN(length) || typeof isBase64 !== 'boolean') {
+        console.error('参数错误');
+        return;
+    }
 
     let md5Hash = input;
     for (let i = 0; i < iteration; i++) {
@@ -18,7 +29,7 @@ async function main(options) {
     if (iteration > 1) {
         logs.push(`迭代 ${iteration} 次,`);
     }
-    if (base64) {
+    if (isBase64) {
         logs.push(`转为 base64,`);
         md5Hash = Buffer.from(md5Hash).toString('base64')
     }
@@ -26,7 +37,7 @@ async function main(options) {
         logs.push(`前面 ${length} 位,`);
         md5Hash = md5Hash.substring(0, length)
     }
-    if (iteration > 1 && base64 && length < 32) {
+    if (iteration > 1 && isBase64 && length < 32) {
         logs.unshift('OK!')
     }
     console.log(...logs, `MD5 哈希值: ${md5Hash}`);
